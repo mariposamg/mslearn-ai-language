3c9f6387bed0219d46bbffca0ad936ded1211a80
AminataM_GUEYE <83471586+mariposamg@users.noreply.github.com>
2025-04-15 18:27:31 +0000

    lab5 Analyze Text

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -12,7 +13,8 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
 
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
@@ -23,19 +25,37 @@ def main():
             print('\n' + text)
 
             # Get language
-
+            detectedLanguage = ai_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
 
             # Get sentiment
-
+            sentiment = ai_client.analyze_sentiment(documents=[text])[0]
+            print('\nSentiment: {}'.format(sentiment.sentiment))
 
             # Get key phrases
+            key_phrases = ai_client.extract_key_phrases(documents=[text])[0]
+            print('\nKey phrases:')
+            for phrase in key_phrases.key_phrases:
+                print(phrase)
 
 
             # Get entities
-
+            entities = ai_client.recognize_entities(documents=[text])[0]
+            print('\nEntities:')
+            for entity in entities.entities:
+                print('\t{}: {}'.format(entity.category, entity.text))  
 
             # Get linked entities
-
+            linked_entities = ai_client.recognize_linked_entities(documents=[text])[0]
+            print('\nLinked entities:')
+            for entity in linked_entities.entities:
+                print('\t{}: {}'.format(entity.data_source, entity.name))
+                print('\t\t{}'.format(entity.url))
+                print('\t\t{}'.format(entity.data_source_entity_id))
+                print('\t\t{}'.format(entity.language))
+                print('\t\t{}'.format(entity.id))
+                print('\t\t{}'.format(entity.bing_id))
+                print('\t\t{}'.format(entity.match_score))      
 
 
     except Exception as ex:
